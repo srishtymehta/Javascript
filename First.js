b028ae459fe9d6733fe5561ab738395751f0c90c
srishtymehta <srishtymehta1@gmail.com>
2018-06-12 17:42:05 +0530

    2.Linked with HtML

---
+++ First.js
@@ -0,0 +1,11 @@
+alert("Hey!!, This is first javaScript File...");
+
+function greetingJavascript() {
+
+    var lol = document.getElementById('lolz').value;
+
+    var s = "hello " + lol + ". Welcome to my Javascript Demo."
+    alert(s);
+    return s;
+    console.log(s);
+}
