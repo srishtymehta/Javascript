c68ae3300cf279dd08b10746fb78aa29e78ba73f
srishtymehta <srishtymehta1@gmail.com>
2018-06-21 15:20:17 +0530

    type-bass

--- Third.js
+++ Third.js
@@ -1,144 +1,90 @@
-let words = ['raw', 'tan', 'man', 'kite', 'abide', 'bait', 'abound', 'music', 'overseas', 'persistent'];
-let scorespan = document.querySelector('#levl');
-let score = 0;
-scorespan.innerHTML = score;
-let levelSpan = document.querySelector('#levl');
-let level = 'First';
-levelSpan.innerHTML = level;
+let begin = document.querySelector("#begin");
+var inter = document.querySelector("#inter");
+var expert = document.querySelector("#expert");
+var score = document.querySelector(".points");
+var display = document.querySelector("#display");
+var text = document.querySelector("#text");
+var start = document.querySelector("#start");
+let time = document.querySelector("#myprogressBar");
 
-// Declare button and input variables
-let input = document.querySelector('#Input');
-let startBtn = document.querySelector('#start');
-let display = document.querySelector('#words');
+var barray = ["look", "cook", "book", "rose", "bait", "lure", "prune", "what", "your", "name", "love", "music", "song", "dua", "bruno", "anne", "bebe", "kygo", "alan", "chris"]
+var iarray = ["selena", "taylor", "rihanna", "miley", "avril", "enable", "disable", "abound", "clayray", "eminem", "arijit", "diljit", "sherya", "sunidhi", "amitabh", "sataya", "katrina", "robin", "ananya", "maroon", "louisa"]
+var earray = ["paulo", "persistant", "imcompetent", "backfire", "vertebrae", "wicked", "wayward", "bewilder", "perplex", "flummox", "receptacle", "boomerang", "capability", "incessantly", "guarantee", "composure", "correspondence", "stabilize", "unfeasible", "prominent"]
+display.innerHTML = "select difficulty";
+
+var array;
+var fscore;
+
+function beg() {
+    array = barray;
+    begin.classList.toggle(true);
+    text.value = "";
+    score.innerHTML = "0";
 
 
-// Function initated when user clicks to start
-function startGame() {
-    display.innerHTML = '';
+}
 
-    // Make sure that the score is set to zero
-    scoreSpan.innerHTML = score;
+function inte() {
+    array = iarray;
+    inter.classList.toggle(true);
+    text.value = "";
+    score.innerHTML = "0";
 
-    //Set position of timer
-    pos = 0;
-    timerSpan.style.left = pos + '%';
 
-    let count = 2;
-    let counter = setInterval(timer, 1000);
+}
 
-    function timer() {
-        display.innerHTML = count;
-        count = count - 1;
-        if (count <= 0) {
-            clearInterval(counter);
-            playGame();
+function exp() {
+    array = earray;
+    var time = document.querySelector("#myprogressBar");
+    expert.classList.toggle('active', true);
+    text.value = "";
+    score.innerHTML = "0";
+
+
+}
+
+function update() {
+    var i = 2;
+    display.innerHTML = 3;
+    var element = document.querySelector("myprogressBar");
+    var width = 1;
+    var identity = setInterval(scene, 10);
+
+    function scene() {
+        display.innerHTML = i;
+        i -= 1;
+        if (width >= 100 && i < 0) {
+            clearInterval(identity);
+            play();
             return;
-        }
-        return;
-    }
-
-}
-
-// Play the Game
-function playGame() {
-    let playing = true;
-
-    if (playing) {
-        let restart = document.querySelector('#restart');
-        let end = document.querySelector('#end');
-
-        // Start game timer
-        gameDuration();
-
-        display.innerHTML = words[0];
-        submitWord();
-        if (restart === 'clicked') {
-            restartGame();
-        }
-        if (end === 'clicked') {
-            endGame();
-        }
-    } else {
-        display.innerHTML = 'Ready?';
-        return;
-    }
-    return;
-}
-
-// Restart the Game 
-function restartGame() {
-    score = 0;
-    scoreSpan.innerHTML = score;
-
-    // Reset Timer
-    pos = 0;
-    timerSpan.style.left = pos + '%'
-
-    startGame();
-    return score;
-}
-
-// End the Game 
-function endGame() {
-    playing = false;
-    display.innerHTML = 'Game Over!';
-
-    if (score <= 20) {
-        alert('Better luck next time! Your score is ' + score + ' points.');
-    } else if (score <= 40) {
-        alert('Awesome job. Your getting there! You scored ' + score + ' points this round.');
-    } else if (score > 50 || score <= 80) {
-        alert('At this rate, you will be typing novels a minute with a score of ' + score + ' points. Bravo!');
-    } else {
-        return;
-    }
-    score = 0;
-    answer.value = '';
-    console.log('The game has ended. Playing set to: ' + playing);
-}
-
-// Game Duration Progress Bar / Span Transform and Color Change
-let timerSpan = document.querySelector("#progressBar");
-let pos = 0;
-
-function gameDuration() {
-    let id = setInterval(frame, 10);
-
-    function frame() {
-        if (pos === 100) {
-            clearInterval(id);
-            pos = 0;
-            endGame();
         } else {
-            pos++;
-            timerSpan.style.left = pos + '%';
-            return pos;
+            width++;
+            element.style.width = width + '%';
         }
     }
 }
 
+function play() {
+    fscore = 0;
+    if (begin === 'clicked') {
+        beg();
+    } else if (inter === 'clicked') {
+        inter();
+    } else if (expert === 'clicked') {
+        exp();
+    } else {
+        display.innerHTML = 'SELECT DIFFICULTY';
+    }
+    display.innerHTML = (array[Math.floor((Math.random() * array.length))]).toLowerCase();
+    if (text.toLowerCase() != (display.innerHTML).toLowerCase()) {
+        clearInterval(indentity1);
+        end();
+        return;
+    }
+}
 
-// Submit the input value and check if word matches the array's iteration
-function submitWord() {
-    let answer = input;
-
-    input.addEventListener('keyup', function () {
-        event.preventDefault;
-        for (i = 0; i <= words.length; i++) {
-            if (answer.value === words[i]) {
-                pos = 0;
-                words[i++];
-                score += 10;
-                scoreSpan.innerHTML = score;
-                display.innerHTML = words[i];
-                answer.value = '';
-                console.log('true');
-                return score;
-            } else {
-                console.log('false');
-            }
-        }
-        return answer;
-    })
-    return answer;
+function end() {
+    display.innerHTML = "Try Again";
+    text.value = "";
+    window.getSelection.empty();
 }
